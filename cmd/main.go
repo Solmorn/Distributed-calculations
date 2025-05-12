5dca1bdfe23d4ff4c9a7496f8cb77088162d77de
Solmorn <matwey.solomatin2007@gmail.com>
2025-05-12 23:17:48 +0300

    base structure

---
+++ cmd/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/Oleg-Neevin/distributed_calculator_final/internal/agent"
+	"github.com/Oleg-Neevin/distributed_calculator_final/internal/db"
+	"github.com/Oleg-Neevin/distributed_calculator_final/internal/orchestrator"
+)
+
+func main() {
+	database := db.GetInstance()
+	defer func() {
+		if err := database.Close(); err != nil {
+			log.Printf("Error close bd: %v", err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		<-sigChan
+		log.Println("Dead signal has been received, close bd...")
+		if err := database.Close(); err != nil {
+			log.Printf("Error close bd: %v", err)
+		}
+		os.Exit(0)
+	}()
+
+	go agent.StartAgent()
+
+	orchestrator.RunOrchestrator()
+
+}
